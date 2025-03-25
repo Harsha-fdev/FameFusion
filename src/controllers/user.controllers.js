b378d3c8f1a0a082dbbc2046196350e5cae47531
Harsha-fdev <harshasshet2002@gmail.com>
2025-03-26 01:56:05 +0530

    Register controller algo successfull.

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,86 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {APIError} from "../utils/APIError.js";
+import {User} from '../models//user.model.js';
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { APIResponse } from "../utils/APIResponse.js";
 
 const registerUser = asyncHandler(async (req , res)=>{
-    res.status(200).json({
-        message:"ok"
+    //steps to register a user
+    //step 1 -> get user-details from frontend
+    //step-2 -> check for validation
+    //step-3 -> check if user alredy exists
+    //step-4 -> check for images , check for avatar
+    //step-5 -> if available upload them to cloudinary(also check avatar once) and extract url
+    //step-6 -> create image object and create entry in db
+    //step-7 -> remove password and refreshtoken from response
+    //step-8 -> check for user creation
+    //step-9 ->return response to frontend
+
+    //step-1
+    const {username , fullname , email , password} = req.body;
+    
+    //step-2
+    // if(fullname === ""){
+    //     throw new APIError(400 , "fullname is required");
+    // };
+    // or
+    if(
+        [fullname , email , username , password].some((field)=>{
+            field?.trim() === ""
+        })
+    ){
+        throw new APIError(400 , "All fields are required")
+    }
+
+    //step-3
+    const existedUser = User.findOne({//you can also use find method
+        $or: [{ username },{ email }]
+    });
+
+    if(existedUser){
+        throw new APIError(409 , "user already exist")
+    }
+
+    //step-4
+    const avatarLocalpath = req.files?.avatar[0]?.path;
+    const coverimageLocalpath = req.files?.coverimage[0]?.path;
+
+    if(!avatarLocalpath){
+        throw new APIError(400 , "Avatar fileis required");
+    }
+
+    //step-5
+    const avatar = await uploadOnCloudinary(avatarLocalpath);
+    const coverimage = await uploadOnCloudinary(coverimageLocalpath);
+
+    if(!avatar){
+        throw new APIError(400 , "Avatar fileis required");
+    }
+
+    //step-6
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverimage: coverimage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase(),
     })
+
+    //step-7
+    const createdUser = await User.findById(user.id).select(
+        "-password -refreshToken"
+    )
+
+    //step-8
+    if(!createdUser){
+        throw new APIError(500 , "Something went wrong while registering user")
+    }
+
+    // step-9
+    return res.status(201).json(
+        new APIResponse(200 , createdUser , "User registered Successfully!!")
+    )
 })
 
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import {upload} from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 //syntax is const.route(path).method
-router.route("/register").post(registerUser)
+//this is howyou inject ,iddleware just before execution of some method
+router.route("/register").post(
+    upload.fields([
+        {name:"avatar",
+            maxCount: 1
+        },
+        {name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
+)
 
 export default router;
